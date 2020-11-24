0213735fab182644ba9579ba1e01bec37c207566
jowenshaw <jowenshaw@gmail.com>
2020-11-24 18:06:39 +0800

    separate reward from staking

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -383,6 +383,7 @@ pragma solidity ^0.5.0;
 
 
 // ANY staking smart contract
+// staking reward is from the owner of this contract
 contract Stake is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
@@ -391,7 +392,7 @@ contract Stake is Ownable {
     struct UserInfo {
         uint256 amount;
         uint256 rewardDebt;
-        bytes32 nodeID;
+        string nodeID;
     }
     mapping (address => UserInfo) public userInfo;
 
@@ -401,24 +402,20 @@ contract Stake is Ownable {
     IERC20 public stakeToken;
     uint256 public rewardPerBlock;
     uint256 public rewardStartBlock;
-    uint256 public bonusEndBlock;
-    uint256 public constant BONUS_MULTIPLIER = 2;
 
     event Deposit(address indexed user, uint256 indexed amount);
     event Withdraw(address indexed user, uint256 indexed amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed amount);
-    event RegisterNode(address indexed user, bytes32 indexed nodeID);
+    event RegisterNode(address indexed user, string nodeID);
 
     constructor(
         IERC20 _stakeToken,
         uint256 _rewardPerBlock,
-        uint256 _rewardStartBlock,
-        uint256 _bonusEndBlock
+        uint256 _rewardStartBlock
     ) public {
         require(_rewardPerBlock > 1e6, "ctor: reward per block is too small");
         stakeToken = _stakeToken;
         rewardPerBlock = _rewardPerBlock;
-        bonusEndBlock = _bonusEndBlock;
         rewardStartBlock = _rewardStartBlock;
 
         lastRewardBlock = block.number > rewardStartBlock ? block.number : rewardStartBlock;
@@ -429,42 +426,29 @@ contract Stake is Ownable {
         rewardPerBlock = _rewardPerBlock;
     }
 
-    // Return reward multiplier over the given _from to _to block.
-    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
-        if (_from <= _to) {
-            return 0;
-        } else if (_to <= bonusEndBlock) {
-            return _to.sub(_from).mul(BONUS_MULTIPLIER);
-        } else if (_from >= bonusEndBlock) {
-            return _to.sub(_from);
-        } else {
-            return bonusEndBlock.sub(_from).mul(BONUS_MULTIPLIER).add(
-                _to.sub(bonusEndBlock)
-            );
-        }
-    }
-
-    function calcTokenReward() internal view returns (uint256) {
+    function calcAccRewardPerShare() public view returns (uint256) {
         if (block.number <= lastRewardBlock) {
-            return 0;
+            return accRewardPerShare;
         }
-        uint256 multiplier = getMultiplier(lastRewardBlock, block.number);
+        uint256 lpSupply = stakeToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            return accRewardPerShare;
+        }
+        uint256 multiplier = block.number.sub(lastRewardBlock);
         uint256 tokenReward = multiplier.mul(rewardPerBlock);
-        return tokenReward;
+        if (tokenReward <= 0) {
+            return accRewardPerShare;
+        }
+        return accRewardPerShare.add(tokenReward.mul(1e12).div(lpSupply));
     }
 
     // View function to see pending REWARDs on frontend.
     function pendingReward(address _user) external view returns (uint256) {
-        uint256 newAccRewardPerShare = accRewardPerShare;
+        uint256 newAccRewardPerShare = calcAccRewardPerShare();
         UserInfo storage user = userInfo[_user];
-        uint256 tokenReward = calcTokenReward();
-        if (tokenReward > 0) {
-            uint256 lpSupply = stakeToken.balanceOf(address(this));
-            newAccRewardPerShare = accRewardPerShare.add(tokenReward.mul(1e12).div(lpSupply));
-        }
-        uint256 pending = user.amount.mul(newAccRewardPerShare).div(1e12);
-        if (pending > user.rewardDebt) {
-            return pending.sub(user.rewardDebt);
+        uint256 reward = user.amount.mul(newAccRewardPerShare).div(1e12);
+        if (reward > user.rewardDebt) {
+            return reward.sub(user.rewardDebt);
         }
         return 0;
     }
@@ -473,21 +457,17 @@ contract Stake is Ownable {
         if (block.number <= lastRewardBlock) {
             return;
         }
-        uint256 tokenReward = calcTokenReward();
-        if (tokenReward > 0) {
-            uint256 lpSupply = stakeToken.balanceOf(address(this));
-            accRewardPerShare = accRewardPerShare.add(tokenReward.mul(1e12).div(lpSupply));
-        }
+        accRewardPerShare = calcAccRewardPerShare();
         lastRewardBlock = block.number;
     }
 
     function farm(UserInfo storage user) internal {
         updatePool();
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(accRewardPerShare).div(1e12);
-            if(pending > user.rewardDebt) {
-                pending = pending.sub(user.rewardDebt);
-                user.amount = user.amount.add(pending);
+            uint256 reward = user.amount.mul(accRewardPerShare).div(1e12);
+            if (reward > user.rewardDebt) {
+                uint256 pending = reward.sub(user.rewardDebt);
+                stakeToken.safeTransferFrom(owner(), address(msg.sender), pending);
             }
         }
     }
@@ -496,7 +476,7 @@ contract Stake is Ownable {
     function deposit(uint256 _amount) public {
         UserInfo storage user = userInfo[msg.sender];
         farm(user);
-        if(_amount > 0) {
+        if (_amount > 0) {
             stakeToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
         }
@@ -507,9 +487,9 @@ contract Stake is Ownable {
     // Withdraw stake tokens
     function withdraw(uint256 _amount) public {
         UserInfo storage user = userInfo[msg.sender];
-        farm(user);
         require(user.amount >= _amount, "not enough balance");
-        if(_amount > 0) {
+        farm(user);
+        if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             stakeToken.safeTransfer(address(msg.sender), _amount);
         }
@@ -528,10 +508,10 @@ contract Stake is Ownable {
     }
 
     // Register node. must have stake
-    function registerNode(bytes32 nodeID) public {
+    function registerNode(string memory _nodeID) public {
         UserInfo storage user = userInfo[msg.sender];
         require(user.amount > 0, "no stake");
-        user.nodeID = nodeID;
-        emit RegisterNode(msg.sender, nodeID);
+        user.nodeID = _nodeID;
+        emit RegisterNode(msg.sender, _nodeID);
     }
 }

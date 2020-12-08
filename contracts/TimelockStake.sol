1e9fd4858a6718bb92e3c94febd985793bc81bf3
jowenshaw <jowenshaw@gmail.com>
2020-12-08 19:14:47 +0800

    add manager and upgrade/downgrade

--- contracts/TimelockStake.sol
+++ contracts/TimelockStake.sol
@@ -382,11 +382,29 @@ pragma solidity ^0.5.0;
 
 
 
+interface IStake {
+    function stakeToken() external view returns (IERC20);
+    function userInfo(address _user) external view returns (uint256 stake, uint256 punish, string memory nodeID);
+    function upper() external view returns (IStake);
+    function lower() external view returns (IStake);
+    function stake(uint256 _amount) external;
+    function unstake(uint256 _amount) external;
+    function importStake(address _user, bool _fromLower) external;
+}
+
 // ANY time lock staking smart contract
 // staking reward is from the owner of this contract
-contract TimelockStake is Ownable {
+contract TimelockStake is Ownable, IStake {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
+
+    IStake private _upperContract;
+    IStake private _lowerContract;
+    address public manager;
+    modifier onlyManager() {
+        require(msg.sender == manager, "only manager");
+        _;
+    }
 
     struct UserInfo {
         uint256 stake;
@@ -416,7 +434,7 @@ contract TimelockStake is Ownable {
     uint256 public lastRewardBlock;
     uint256 public accRewardPerShare;
 
-    IERC20 public stakeToken;
+    IERC20 private _stakeToken; // ANY token with decimals 18
     uint256 public stakePieceAmount = 5*1e21;
     uint256 public stakeCycleStartTime;
     uint256 public stakeCycleInterval = 30*24*3600;
@@ -435,18 +453,21 @@ contract TimelockStake is Ownable {
     event AddPunish(address indexed user, uint256 indexed offlineHours, uint256 indexed amount);
     event PunishFromReward(address indexed user, uint256 indexed amount);
     event PunishFromStake(address indexed user, uint256 indexed amount);
+    event UpgradeNode(address indexed from, address indexed to, string nodeID);
+    event DowngradeNode(address indexed from, address indexed to, string nodeID);
+    event SetManager(address indexed oldManager, address indexed newManager);
 
     constructor(
-        IERC20 _stakeToken,
+        IERC20 _stakeTokenAddr,
         uint256 _rewardPerBlock,
         uint256 _rewardStartBlock,
         uint256 _stakeCycleStartTime,
         uint256 _stakePieceAmount
     ) public {
-        require(_rewardPerBlock > 1e6, "ctor: reward per block is too small");
+        require(_rewardPerBlock == 0 || _rewardPerBlock > 1e6, "ctor: reward per block is too small");
         require(_stakeCycleStartTime + stakeCycleInterval > block.timestamp, "ctor: outdated cycle");
 
-        stakeToken = _stakeToken;
+        _stakeToken = _stakeTokenAddr;
         rewardPerBlock = _rewardPerBlock;
         rewardStartBlock = _rewardStartBlock;
         stakeCycleStartTime =  _stakeCycleStartTime;
@@ -455,6 +476,21 @@ contract TimelockStake is Ownable {
         }
 
         lastRewardBlock = block.number > rewardStartBlock ? block.number : rewardStartBlock;
+
+        manager = msg.sender;
+        emit SetManager(address(0), manager);
+    }
+
+    function stakeToken() public view returns (IERC20) {
+        return _stakeToken;
+    }
+
+    function upper() public view returns (IStake) {
+        return _upperContract;
+    }
+
+    function lower() public view returns (IStake) {
+        return _lowerContract;
     }
 
     function userInfo(address _user) public view returns (uint256 stakeAmount, uint256 punishAmount, string memory nodeID) {
@@ -492,8 +528,23 @@ contract TimelockStake is Ownable {
         locked = block.timestamp > start.add(stakeFreeInterval);
     }
 
+    function setUpper(IStake _upper) public onlyOwner {
+        require(address(_upper) == address(0) || _upper.stakeToken() == _stakeToken, "invalid stake token");
+        _upperContract = _upper;
+    }
+
+    function setLower(IStake _lower) public onlyOwner {
+        require(address(_lower) == address(0) || _lower.stakeToken() == _stakeToken, "invalid stake token");
+        _lowerContract = _lower;
+    }
+
+    function setManager(address _manager) public onlyOwner {
+        emit SetManager(manager, _manager);
+        manager = _manager;
+    }
+
     function setStakeCycle(uint256 _startTime, uint256 _cycleInterval, uint256 _freeInterval) public onlyOwner {
-        require(_cycleInterval > _freeInterval, "wrong interval");
+        require(_cycleInterval >= _freeInterval, "wrong interval");
         require(_startTime.add(_cycleInterval) > block.timestamp, "outdated cycle");
         stakeCycleStartTime = _startTime;
         stakeCycleInterval = _cycleInterval;
@@ -513,7 +564,7 @@ contract TimelockStake is Ownable {
         punishAmountPerHour = _amount;
     }
 
-    function punish(address _user, uint256 _offlineHours) public onlyOwner {
+    function punish(address _user, uint256 _offlineHours) public onlyManager {
         require(punishAmountPerHour > 0, "no punish");
         require(_offlineHours > 0, "zero time");
         UserInfo storage user = _userInfo[_user];
@@ -527,7 +578,7 @@ contract TimelockStake is Ownable {
         if (block.number <= lastRewardBlock) {
             return accRewardPerShare;
         }
-        uint256 lpSupply = stakeToken.balanceOf(address(this));
+        uint256 lpSupply = _stakeToken.balanceOf(address(this));
         if (lpSupply == 0) {
             return accRewardPerShare;
         }
@@ -592,7 +643,7 @@ contract TimelockStake is Ownable {
         uint256 reward = user.rewardBalance;
         if (reward > 0) {
             user.rewardBalance = 0;
-            stakeToken.safeTransferFrom(owner(), address(msg.sender), reward);
+            _stakeToken.safeTransferFrom(owner(), address(msg.sender), reward);
         }
         user.rewardDebt = user.stake.mul(accRewardPerShare).div(1e12);
         emit TakeReward(msg.sender, reward);
@@ -601,8 +652,9 @@ contract TimelockStake is Ownable {
     function stake(uint256 _amount) public {
         require(_amount > 0 && _amount.mod(stakePieceAmount) == 0, "wrong stake amount");
         UserInfo storage user = _userInfo[msg.sender];
+        require(user.stake > 0 || address(_lowerContract) == address(0), "can not stake");
         _farm(user);
-        stakeToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+        _stakeToken.safeTransferFrom(address(msg.sender), address(this), _amount);
         user.stake = user.stake.add(_amount);
         user.rewardDebt = user.stake.mul(accRewardPerShare).div(1e12);
         emit Stake(msg.sender, _amount);
@@ -621,7 +673,7 @@ contract TimelockStake is Ownable {
         emit RequestUnstake(msg.sender, _amount, block.timestamp);
     }
 
-    function rejectUnstake(address _user) public onlyOwner {
+    function rejectUnstake(address _user) public onlyManager {
         _updateStakeCycle();
         UnstakeInfo storage unstake = _unstakeInfo[_user];
         if (unstake.requestUnstakeTime > stakeCycleStartTime) {
@@ -630,7 +682,7 @@ contract TimelockStake is Ownable {
         }
     }
 
-    function allowUnstake(address _user) public onlyOwner {
+    function allowUnstake(address _user) public onlyManager {
         _updateStakeCycle();
         UnstakeInfo storage unstake = _unstakeInfo[_user];
         if (unstake.requestUnstakeTime > stakeCycleStartTime) {
@@ -639,7 +691,7 @@ contract TimelockStake is Ownable {
         }
     }
 
-    function banUnstake(address _user, bool _banned) public onlyOwner {
+    function banUnstake(address _user, bool _banned) public onlyManager {
         UnstakeInfo storage unstake = _unstakeInfo[_user];
         unstake.banned = _banned;
         emit BanUnstake(_user, _banned);
@@ -661,6 +713,10 @@ contract TimelockStake is Ownable {
         // not enough stake amount
         if (_user.stake < _amount) {
             return false;
+        }
+        // always lock free
+        if (stakeCycleInterval == stakeFreeInterval) {
+            return true;
         }
         // not enough request amount
         if (_unstake.requestUnstakeAmount < _amount) {
@@ -713,13 +769,14 @@ contract TimelockStake is Ownable {
             user.rewardDebt = user.stake.mul(accRewardPerShare).div(1e12);
         }
         if (_amount > 0) {
-            stakeToken.safeTransfer(address(msg.sender), _amount);
+            _stakeToken.safeTransfer(address(msg.sender), _amount);
         }
         emit Unstake(msg.sender, _amount);
     }
 
     function registerNode(string memory _nodeID) public {
-        require(bytes(_nodeID).length > 0, "invalid nodeID");
+        require(address(_lowerContract) == address(0), "can not register");
+        require(bytes(_nodeID).length > 0 && bytes(_nodeID).length <= 256, "invalid nodeID");
         NodeInfo storage nodes = _nodeInfo[_nodeID];
         require(nodes.owner == address(0), "already registered");
         UserInfo storage user = _userInfo[msg.sender];
@@ -731,5 +788,74 @@ contract TimelockStake is Ownable {
         nodes.owner = msg.sender;
         nodes.registerTime = block.timestamp;
         emit RegisterNode(msg.sender, _nodeID);
+    }
+
+    function setNode(address _user, string memory _nodeID) public onlyOwner {
+        require(bytes(_nodeID).length > 0 && bytes(_nodeID).length <= 256, "invalid nodeID");
+        NodeInfo storage nodes = _nodeInfo[_nodeID];
+        UserInfo storage user = _userInfo[_user];
+        require(user.stake > 0, "no stake");
+        if (bytes(user.nodeID).length > 0) {
+            delete _nodeInfo[user.nodeID]; // delete old
+        }
+        user.nodeID = _nodeID;
+        nodes.owner = _user;
+        nodes.registerTime = block.timestamp;
+        emit RegisterNode(_user, _nodeID);
+    }
+
+    function importStake(address _user, bool _fromLower) public {
+        UserInfo storage user = _userInfo[_user];
+        require(user.stake == 0 && bytes(user.nodeID).length == 0, "already exist");
+        require((_fromLower && _lowerContract.upper() == IStake(this)) ||
+                (!_fromLower && _upperContract.lower() == IStake(this)), "not linked");
+        IStake from = _fromLower ? _lowerContract : _upperContract;
+        require(msg.sender == address(from), "wrong caller");
+        (uint256 stakeAmount, uint256 punishAmount, string memory nodeID) = from.userInfo(_user);
+        user.stake = stakeAmount;
+        user.punish = punishAmount;
+        user.nodeID = nodeID;
+        NodeInfo storage nodes = _nodeInfo[nodeID];
+        require(nodes.owner == address(0) || nodes.registerTime == 1, "already exist");
+        nodes.owner = _user;
+        nodes.registerTime = block.timestamp;
+        updatePool();
+        user.rewardDebt = user.stake.mul(accRewardPerShare).div(1e12);
+    }
+
+    function upgradeNode(string memory _nodeID) public onlyManager {
+        require(address(_upperContract) != address(0) && _upperContract.lower() == IStake(this), "not linked");
+        migrateNode(_nodeID, true);
+        emit UpgradeNode(address(this), address(_upperContract), _nodeID);
+    }
+
+    function downgradeNode(string memory _nodeID) public onlyManager {
+        require(address(_lowerContract) != address(0) && _lowerContract.upper() == IStake(this), "not linked");
+        migrateNode(_nodeID, false);
+        emit DowngradeNode(address(this), address(_lowerContract), _nodeID);
+    }
+
+    function migrateNode(string memory _nodeID, bool _upgrade) internal {
+        NodeInfo storage nodes = _nodeInfo[_nodeID];
+        require(nodes.owner != address(0), "not registered");
+        UserInfo storage user = _userInfo[nodes.owner];
+        require(user.stake > 0, "no stake");
+
+        _farm(user);
+        IStake to = _upgrade ? _upperContract : _lowerContract;
+        to.importStake(nodes.owner, !_upgrade);
+        _stakeToken.safeTransfer(address(to), user.stake);
+
+        // clearing of storage
+        user.stake = 0;
+        user.punish = 0;
+        user.rewardDebt = 0;
+        delete _unstakeInfo[msg.sender];
+        if (address(_lowerContract) == address(0)) {
+            _nodeInfo[_nodeID].registerTime = 1; // set upgrade flag
+        } else {
+            delete _nodeInfo[_nodeID];
+            user.nodeID = "";
+        }
     }
 }
